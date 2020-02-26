cc417648ddd2a709b149d7023e230a96ecf80f79
c.esneault <c.esneault@oodrive.com>
2020-02-26 14:04:25 +0100

    Add signature to Sparkle, and hardened runtime

--- SparkleSharp/build_framework.sh
+++ SparkleSharp/build_framework.sh
@@ -6,4 +6,14 @@ git submodule init && git submodule update
 cd 3rdparty/Sparkle
 xcodebuild -configuration Release -target bsdiff
 xcodebuild -configuration Release -target ed25519
-xcodebuild -configuration Release -target Sparkle
+xcodebuild -configuration Release -target Sparkle -xcconfig ../../release.xcconfig
+
+CodeSigningCertificate=`security find-identity -v -p codesigning | grep "Developer ID Application:" | awk -F'"' '{print $2}'`
+echo Signing with certificate: \"$CodeSigningCertificate\"
+
+# Some Sparkle dependencies did not support hardened runtime, but it can be set during code signing with "runtime" option.
+# https://github.com/micahstubbs/open-dozer/issues/4
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate"  build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate"  build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app/Contents/MacOS/fileop
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate"  build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app/Contents/MacOS/Autoupdate
+
