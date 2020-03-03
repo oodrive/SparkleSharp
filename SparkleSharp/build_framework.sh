61669ce64af3cd6c38880fc1a10473d35c0bddb6
c.esneault <c.esneault@oodrive.com>
2020-03-03 16:09:05 +0100

    Add missing signature

--- SparkleSharp/build_framework.sh
+++ SparkleSharp/build_framework.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR=$(dirname ${0})
+echo Building sparkle from ${DIR}
 cd ${DIR}
 git submodule init && git submodule update
 cd 3rdparty/Sparkle
@@ -13,7 +14,12 @@ echo Signing with certificate: \"$CodeSigningCertificate\"
 
 # Some Sparkle dependencies did not support hardened runtime, but it can be set during code signing with "runtime" option.
 # https://github.com/micahstubbs/open-dozer/issues/4
-codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate"  build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app
-codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate"  build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app/Contents/MacOS/fileop
-codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate"  build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app/Contents/MacOS/Autoupdate
 
+# WARNING: codsign order is important, always sign children before parents!
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate" build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app/Contents/MacOS/fileop
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate" build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app/Contents/MacOS/Autoupdate
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate" build/Release/Sparkle.framework/Versions/Current/Resources/Autoupdate.app
+codesign --force --deep --verbose --timestamp --options runtime --sign "$CodeSigningCertificate" build/Release/Sparkle.framework/Versions/Current/.
+
+# TODO: use Sharpie to generate obj-c wrapper
+#sharpie bind -o "${DIR}" -scope build/Release/Sparkle.framework/Versions/A/Headers  -namespace SparkleSharp -sdk macosx10.14 build/Release/Sparkle.framework/Versions/A/Headers/Sparkle.h
